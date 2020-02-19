1c41bbe13d410fcaad323e3e58ea92e5736c5668
cmily <1786386686@qq.com>
2020-02-19 11:27:46 +0800

    225

---
+++ leecode/225-用队列实现栈.py
@@ -0,0 +1,39 @@
+import collections
+
+
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.q = collections.deque()
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.q.appendleft(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.q.popleft()
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.q[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return not len(self.q)
+
+
+a = MyStack()
+a.push(1)
+print(a.pop())
