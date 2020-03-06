661ccaf1e0fcfafc787334242f61bbdb11f75b51
cmily <1786386686@qq.com>
2020-03-06 13:05:22 +0800

    3-6

--- leecode/60-第60个排列.py
+++ leecode/60-第60个排列.py
@@ -1,2 +1,5 @@
-a = ['1', '2', '3']
-print([*map(int, a)])
+def getPermutation(self, n, k):
+    from itertools import permutations
+    tup = list(permutations(list(range(1, n+1)), n))[k-1]
+    tup = list(map(str, tup))
+    return ''.join(tup)
