b2c5952e7fee100c346f9bf790826d3a46811462
cmily <1786386686@qq.com>
2020-03-09 10:49:13 +0800

    3-9

---
+++ leecode/121-买卖股票的最佳时机.py
@@ -0,0 +1,7 @@
+def maxProfit(self, prices):
+    minprice = float('INF')
+    maxprofit = 0
+    for price in prices:
+        maxprofit = max(price - minprice, maxprofit)
+        minprice = min(price, minprice)
+    return maxprofit
