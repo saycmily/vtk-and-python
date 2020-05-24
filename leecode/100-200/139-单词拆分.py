206d6526c119ff6df1a080ce424a99f16135f24e
cmily <1786386686@qq.com>
2020-05-24 10:42:31 +0800

    5-24

---
+++ leecode/100-200/139-单词拆分.py
@@ -0,0 +1,11 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict) -> bool:
+        if not s:
+            return True
+        breakp = [0]
+        for i in range(len(s) + 1):
+            for j in breakp:
+                if s[j:i] in wordDict:
+                    breakp.append(i)
+                    break
+        return breakp[-1] == len(s)
