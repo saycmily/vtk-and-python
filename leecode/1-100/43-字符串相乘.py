5712268a545f90b619ffdb4f8cedca755e517b9c
cmily <1786386686@qq.com>
2020-08-13 02:01:04 +0800

    8-13

--- leecode/1-100/43-字符串相乘.py
+++ leecode/1-100/43-字符串相乘.py
@@ -1,4 +1,8 @@
-def multiply(self, num1, num2):
-    num1 = int(num1)
-    num2 = int(num2)
-    return str(num1*num2)
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        n1, n2 = 0, 0
+        for i in num1:
+            n1 = n1*10 + ord(i) - 48
+        for i in num2:
+            n2 = n2*10 + ord(i) - 48
+        return str(n1*n2)
